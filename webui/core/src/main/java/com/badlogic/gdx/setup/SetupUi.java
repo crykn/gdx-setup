24cdeabeaa39303ea7d65ca87ad24fadd000eb1c
Benjamin Schulte <bs@golfgl.de>
2020-10-04 21:52:12 +0200

    Added webui

---
+++ webui/core/src/main/java/com/badlogic/gdx/setup/SetupUi.java
@@ -0,0 +1,22 @@
+package com.badlogic.gdx.setup;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+
+public class SetupUi extends ApplicationAdapter {
+
+
+    @Override
+    public void create() {
+
+    }
+
+    @Override
+    public void render() {
+        Gdx.gl.glClearColor(0, 0, 0, 0);
+        Gdx.gl.glClear(Gdx.gl.GL_COLOR_BUFFER_BIT);
+
+    }
+
+
+}
