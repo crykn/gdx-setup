e28bc34604950689f66a4db0cca3d5d6fc9e0e3c
raeleus <raymond.ray3k@gmail.com>
2020-10-05 21:19:08 -0700

    WebUI: Add hide animation.

--- webui/core/src/main/java/com/badlogic/gdx/setup/tables/LoadingTable.java
+++ webui/core/src/main/java/com/badlogic/gdx/setup/tables/LoadingTable.java
@@ -1,12 +1,47 @@
 package com.badlogic.gdx.setup.tables;
 
-import com.badlogic.gdx.scenes.scene2d.ui.*;
+import com.badlogic.gdx.scenes.scene2d.actions.Actions;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.ray3k.tenpatch.TenPatchDrawable;
 
 import static com.badlogic.gdx.setup.SetupUi.*;
 
 public class LoadingTable extends Table  {
+    TenPatchDrawable tenPatchDrawable;
+    Mode mode;
+    public enum Mode {
+        LOADING, HIDE, HIDING, DONE
+    }
+
     public LoadingTable() {
-        Image image = new Image(skin, "loading-animation");
+        tenPatchDrawable = new TenPatchDrawable(skin.get("loading-animation", TenPatchDrawable.class));
+        Image image = new Image(tenPatchDrawable);
         add(image);
+        mode = Mode.LOADING;
+
+        addAction(Actions.delay(5.0f, Actions.run(() -> {
+            mode = Mode.HIDE;
+        })));
+    }
+
+    @Override
+    public void act(float delta) {
+        super.act(delta);
+        if (tenPatchDrawable.getRegions().peek().equals(tenPatchDrawable.getKeyFrame())) {
+            switch (mode) {
+                case HIDE:
+                    mode = Mode.HIDING;
+                    clearChildren();
+                    tenPatchDrawable = new TenPatchDrawable(skin.get("loading-hide", TenPatchDrawable.class));
+                    Image image = new Image(tenPatchDrawable);
+                    add(image);
+                    break;
+                case HIDING:
+                    clearChildren();
+                    mode = Mode.DONE;
+                    break;
+            }
+        }
     }
 }
