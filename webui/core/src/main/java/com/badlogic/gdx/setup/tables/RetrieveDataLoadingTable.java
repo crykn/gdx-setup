30ccffdce6efa974baa649230fdae67ad43e410e
raeleus <raymond.ray3k@gmail.com>
2020-10-07 22:16:35 -0700

    WebUI: Added status message to RetrieveDataLoadingTable

--- webui/core/src/main/java/com/badlogic/gdx/setup/tables/RetrieveDataLoadingTable.java
+++ webui/core/src/main/java/com/badlogic/gdx/setup/tables/RetrieveDataLoadingTable.java
@@ -1,9 +1,12 @@
 package com.badlogic.gdx.setup.tables;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.setup.backend.BackendClient;
 import com.badlogic.gdx.setup.backend.VersionResponse;
+import com.badlogic.gdx.utils.Align;
 import com.ray3k.tenpatch.TenPatchDrawable;
 
 import static com.badlogic.gdx.setup.SetupUi.*;
@@ -11,6 +14,7 @@ import static com.badlogic.gdx.setup.SetupUi.*;
 public class RetrieveDataLoadingTable extends Table  {
     WaitForResponseListener<VersionResponse> versionResponse;
     TenPatchDrawable tenPatchDrawable;
+    Label label;
     Mode mode;
     public enum Mode {
         REQUESTING, SUCCESS, FAIL, HIDING, DONE
@@ -23,7 +27,15 @@ public class RetrieveDataLoadingTable extends Table  {
         tenPatchDrawable = new TenPatchDrawable(skin.get("loading-animation", TenPatchDrawable.class));
         Image image = new Image(tenPatchDrawable);
         add(image);
+    
+        row();
+        label = new Label("", skin, "loading");
+        label.setWrap(true);
+        label.setAlignment(Align.center);
+        add(label).growX();
+        
         mode = Mode.REQUESTING;
+        label.setText("REQUESTING VERSION");
     }
 
     @Override
@@ -33,17 +45,21 @@ public class RetrieveDataLoadingTable extends Table  {
             switch (mode) {
                 case SUCCESS:
                     mode = Mode.HIDING;
+                    label.setText("LOADING UI");
+                    
                     clearChildren();
                     tenPatchDrawable = new TenPatchDrawable(skin.get("loading-hide", TenPatchDrawable.class));
                     Image image = new Image(tenPatchDrawable);
                     add(image);
+                    row();
+                    add(label);
                     
                     supportedGDXVersions = versionResponse.retrievedData.supportedGdxVersions;
                     buildVersion = versionResponse.retrievedData.backendVersion;
                     break;
                 case HIDING:
-                    clearChildren();
                     mode = Mode.DONE;
+                    
                     landingTable.populate();
                     classicProjectTable.populate();
                     slideDownTable(landingTable);
@@ -54,7 +70,6 @@ public class RetrieveDataLoadingTable extends Table  {
     
     private class WaitForResponseListener<T> implements BackendClient.IBackendResponse<T> {
         T retrievedData;
-        int lastCode;
         
         WaitForResponseListener() {
             mode = Mode.REQUESTING;
@@ -62,15 +77,18 @@ public class RetrieveDataLoadingTable extends Table  {
         
         @Override
         public void onFail(int statusCode, String errorMsg) {
-            mode = Mode.FAIL;
-            lastCode = statusCode;
+            Gdx.app.postRunnable(() -> {
+                mode = Mode.FAIL;
+                label.setText("CANNOT CONNECT TO SERVER\nERROR CODE " + statusCode);
+            });
         }
         
         @Override
         public void onSuccess(T retrievedData) {
             this.retrievedData = retrievedData;
-            lastCode = 0;
-            mode = Mode.SUCCESS;
+            Gdx.app.postRunnable(() -> {
+                mode = Mode.SUCCESS;
+            });
         }
     }
 }
